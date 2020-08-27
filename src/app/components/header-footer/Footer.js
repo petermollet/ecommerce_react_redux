8dcd0be5a18814dbbf08c4690d6275bd61ce391b
Peter Mollet <peter.mollet@neuf.fr>
2020-08-27 14:31:44 +0200

    PM - modification url github footer with my official github

--- src/app/components/header-footer/Footer.js
+++ src/app/components/header-footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="text-center">
-                <span className="text-muted"><a href='https://github.com/plisken59?tab=repositories'>@Peter Mollet</a> - 2020 - <a href='http://www.insy2s.com/'>INSY2S</a></span>
+                <span className="text-muted"><a href='https://github.com/petermollet?tab=repositories'>@Peter Mollet</a> - 2020 - <a href='http://www.insy2s.com/'>INSY2S</a></span>
             </div>
         </footer>
     );
