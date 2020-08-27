d93baeb64b21bb0bee9a26218f026c31562d7e50
Peter Mollet <peter.mollet@neuf.fr>
2020-08-27 14:32:55 +0200

    PM - réindentation

--- src/app/components/header-footer/Footer.js
+++ src/app/components/header-footer/Footer.js
@@ -4,7 +4,11 @@ const Footer = () => {
     return (
         <footer>
             <div className="text-center">
-                <span className="text-muted"><a href='https://github.com/petermollet?tab=repositories'>@Peter Mollet</a> - 2020 - <a href='http://www.insy2s.com/'>INSY2S</a></span>
+                <span className="text-muted">
+                    <a href='https://github.com/petermollet?tab=repositories'>@Peter Mollet</a> 
+                    - 2020 - 
+                    <a href='http://www.insy2s.com/'>INSY2S</a>
+                </span>
             </div>
         </footer>
     );
